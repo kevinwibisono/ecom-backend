e88837ed8fab288b757b743b5e5f8410a0267169
Kevin Alexander <kevin.aw2204@gmail.com>
2020-12-10 22:14:15 +0700

    Perbaiki lagi endpoint testing ipaymu

--- routes/transaksi.js
+++ routes/transaksi.js
@@ -154,7 +154,12 @@ router.post("/afterIpaymu", async function(req, res){
 });
 
 router.post("/testingIpaymu", async function(req, res){
-    res.send("Body: ",req.body,"Query: ",req.query);
+    if(req.body){
+        res.status(200).send(req.body);
+    }
+    else{
+        res.status(200).send(req.query);
+    }
 });
 
 module.exports = router;
